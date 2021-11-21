3cb2dccfe490c3be0145eaddc22b26ec1b5b1374
Chris Ho <ho.christopher.lew@gmail.com>
2021-11-21 16:55:20 -0500

    Added board grid, need to fill in structure for handling key presses and collision detection

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ def print_hi(name):
 
 class Tetromino:
     def __init__(self):
-        self.coordinates = []
+        self.block_coordinates = []
         self.orientation = 0
 
-    def define_coordinates(self):
+    def define_block_coordinates(self):
         if self.shape == "L":
             if self.orientation ==0 or self.orientation ==2:
-                self.coordinates = [[0,0],[0,1],[0,2],[0,3]]
+                self.block_coordinates = [[0,0],[0,1],[0,2],[0,3]]
             elif self.orientation==1 or self.orientation ==3:
-                self.coordinates = [[0, 0], [1, 0], [2, 0], [3, 0]]
+                self.block_coordinates = [[0, 0], [1, 0], [2, 0], [3, 0]]
 
     def rotate(self):
         self.orientation +=1
         if self.orientation>3:
             self.orientation = 0
-        self.define_coordinates(self)
+        self.define_block_coordinates(self)
 
 class L_tet(Tetromino):
     def __init__(self):
-        self.coordinates = np.array([[0,0],[0,1],[0,2],[1,0]])
+        self.block_coordinates = np.array([[0,0],[0,1],[0,2],[1,0]])
         self.orientation = 0
         self.position = np.array([5,5])
 
-    def define_coordinates(self):
+    def define_block_coordinates(self):
         if self.orientation == 0 or self.orientation == 2:
-            self.coordinates = np.array([[0, 0], [0, 1], [0, 2], [0, 3]])
+            self.block_coordinates = np.array([[0, 0], [0, 1], [0, 2], [0, 3]])
         elif self.orientation == 1 or self.orientation == 3:
-            self.coordinates = np.array([[0, 0], [1, 0], [2, 0], [3, 0]])
+            self.block_coordinates = np.array([[0, 0], [1, 0], [2, 0], [3, 0]])
 
 class Tetris:
 
@@ -44,16 +44,34 @@ class Tetris:
         self.display = pygame.display.set_mode(dispbound)
         self.game_window = [200,200]
         self.quit_game = False
-        self.block_size =10
         self.velocity = np.array([0,-1])
+        self.board_dimensions = np.array([10, 20])
+        self.board_position = np.array([50,50])
 
     def displaypiece(self,piece):
-        size_arr = np.array([self.block_size,self.block_size])
-        for coord in piece.coordinates:
-            coord_pos = (piece.position + coord) * self.block_size
+        size_arr = np.array([block_size,block_size])
+        for coord in piece.block_coordinates:
+            coord_pos = (piece.position + coord) * block_size
             block_Rect = [coord_pos, size_arr]
             pygame.draw.rect(self.display, BLUE, block_Rect)
 
+
+    def drawboard(self):
+        board_bounds = self.board_dimensions *block_size
+        boundary_rect = [self.board_position ,board_bounds]
+        pygame.draw.rect(self.display,BLACK,boundary_rect,3)
+        for x_idx in range(0,self.board_dimensions[0]):
+            start = [self.board_position[0]+x_idx *block_size, self.board_position[1]]
+            end = [self.board_position[0] + x_idx *block_size, self.board_position[1] + block_size*self.board_dimensions[1]]
+            pygame.draw.line(self.display,BLACK,start,end)
+        for y_idx in range(0,self.board_dimensions[1]):
+            start = [self.board_position[0],self.board_position[1] + y_idx*block_size]
+            end = [self.board_position[0]+ block_size*self.board_dimensions[0],
+                   self.board_position[1]+y_idx*block_size]
+            pygame.draw.line(self.display, BLACK, start, end)
+
+    def detectcollision(self,piece):
+        return
 
     def mainloop(self):
         my_piece = L_tet()
@@ -63,9 +81,18 @@ class Tetris:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit_game = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        return
+                    if event.key == pygame.K_RIGHT:
+                        return
+                    if event.key == pygame.K_UP:
+                        return
+                    if event.key == pygame.K_DOWN:
+                        return
 
             self.displaypiece(my_piece)
-
+            self.drawboard()
             my_piece.position -= self.velocity
             pygame.display.update()
 
@@ -87,6 +114,7 @@ if __name__ == '__main__':
     BLUE = (0, 0, 255)
     RED = (255, 0, 0)
     GREEN = (0, 128, 0)
+    block_size = 10
 
     clock = pygame.time.Clock()
 
